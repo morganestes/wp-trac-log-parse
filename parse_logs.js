9476fed215c72bdd9a1ddf79d89877ed8cf37ba5
Morgan Estes <morgan.estes@gmail.com>
2015-10-22 17:03:37 -0500

    Changed to dot notation for accessing object properties
    Continuing JSCS updates by switching to dot notation instead of array-like
    notation for accessing object properties. Still use bracket notation
    when the key is generated dynamically.

--- parse_logs.js
+++ parse_logs.js
@@ -26,8 +26,8 @@ function buildChangesets(buildCallback) {
       break;
     }
 
-    changeset['revision'] = $(logEntries[i]).find('td.rev').text().trim().replace(/@(.*)/, '[$1]');
-    changeset['author'] = $(logEntries[i]).find('td.author').text().trim();
+    changeset.revision = $(logEntries[i]).find('td.rev').text().trim().replace(/@(.*)/, '[$1]');
+    changeset.author = $(logEntries[i]).find('td.author').text().trim();
 
     description = $(logEntries[i + 1]).find('td.log');
 
@@ -37,35 +37,35 @@ function buildChangesets(buildCallback) {
     });
 
     // Store "Fixes" or "See" tickets.
-    changeset['related'] = [];
-    changeset['component'] = [];
+    changeset.related = [];
+    changeset.component = [];
     $(description).find('a.ticket').each(function () {
       var ticket = $(this).text().trim().replace(/#(.*)/, '$1');
-      changeset['related'].push(ticket);
+      changeset.related.push(ticket);
     });
 
     // Create base description
-    changeset['description'] = description.text();
+    changeset.description = description.text();
 
     // For now, get rid of Fixes and See notes. Should we annotate in summary?
-    changeset['description'] = changeset['description'].replace(/[\n|, ]Fixes(.*)/i, '');
-    changeset['description'] = changeset['description'].replace(/\nSee(.*)/i, '');
+    changeset.description = changeset.description.replace(/[\n|, ]Fixes(.*)/i, '');
+    changeset.description = changeset.description.replace(/\nSee(.*)/i, '');
 
     // Extract Props
     var propsRegex = /\nProps(.*)./i;
-    changeset['props'] = [];
+    changeset.props = [];
 
-    var props = changeset['description'].match(propsRegex);
+    props = changeset.description.match(propsRegex);
     if (props !== null) {
-      changeset['props'] = props[1].trim().split(/\s*,\s*/);
+      changeset.props = props[1].trim().split(/\s*,\s*/);
     }
 
     // Remove Props
-    changeset['description'] = changeset['description'].replace(propsRegex, '');
+    changeset.description = changeset.description.replace(propsRegex, '');
 
     // Limit to 2 consecutive carriage returns
-    changeset['description'] = changeset['description'].replace(/\n\n\n+/g, '\n\n');
-    changeset['description'] = changeset['description'].trim();
+    changeset.description = changeset.description.replace(/\n\n\n+/g, '\n\n');
+    changeset.description = changeset.description.trim();
 
     changesets.push(changeset);
   }
@@ -77,11 +77,11 @@ function gatherComponents(gatherCallback) {
   var ticketPath = 'https://core.trac.wordpress.org/ticket/';
 
   async.each(changesets, function (changeset, changesetCallback) {
-        async.each(changeset['related'], function (ticket, relatedCallback) {
+        async.each(changeset.related, function (ticket, relatedCallback) {
           request(ticketPath + ticket, function (err, response, body) {
             if (!err && response.statusCode == 200) {
               component = $.load(body)('#h_component').next('td').text().trim();
-              changeset['component'].push(component);
+              changeset.component.push(component);
             }
             relatedCallback();
           });
@@ -116,7 +116,7 @@ function buildOutput(outputCallback) {
 
   async.map(changesets,
       function (item) {
-        category = item['component'];
+        category = item.component;
 
         if (!category) {
           category = 'Misc';
@@ -126,26 +126,26 @@ function buildOutput(outputCallback) {
           categories[category] = [];
         }
 
-        categories[item['component']].push(item);
+        categories[item.component].push(item);
       }
   );
 
   _.each(categories, function (category) {
-    changesetOutput += "### " + category[0]['component'] + "\n";
+    changesetOutput += "### " + category[0].component + "\n";
     _.each(category, function (changeset) {
 
       changesetOutput += '* ' +
-          changeset['description'].trim() + ' ' +
-          changeset['revision'] + ' ' +
-          '#' + changeset['related'].join(', #') + "\n";
+          changeset.description.trim() + ' ' +
+          changeset.revision + ' ' +
+          '#' + changeset.related.join(', #') + "\n";
 
       // Make sure Committers get credit
-      props.push(changeset['author']);
+      props.push(changeset.author);
 
       // Sometimes Committers write their own code.
       // When this happens, there are no additional props.
-      if (changeset['props'].length != 0) {
-        props = props.concat(changeset['props']);
+      if (changeset.props.length != 0) {
+        props = props.concat(changeset.props);
       }
 
     });
